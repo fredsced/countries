974f72878436ee17b4b54db4a28babb1c00c94ae
Frédéric Descloux <frederic.descloux@gmail.com>
2020-08-18 11:39:34 +0200

    add gitignore

--- scripts/env.js
+++
@@ -1,7 +0,0 @@
-// URL to obtain EU countries with only the required fields
-const COUNTRIES_URL = 'https://restcountries.eu/rest/v2/regionalbloc/eu?fields=name;capital;population;area;flag;borders;'
-
-const MAP_URL = 'https://www.google.com/maps/embed/v1/place';
-const APIKEY = 'AIzaSyCnYhHe2_GKIyz_2gjHG__qxEecQ7VyLvs';
-
-export { COUNTRIES_URL, MAP_URL, APIKEY}
