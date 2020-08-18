2feffc94ec6488b52209c84cc7e0101365e925ab
Frédéric Descloux <frederic.descloux@gmail.com>
2020-08-18 10:52:29 +0200

    refactor: extract business logic from fetch

--- scripts/index.js
+++ scripts/index.js
@@ -1,52 +1,58 @@
 
 // URL to obtain EU countries with only the required fields
-const countriesURL = 'https://restcountries.eu/rest/v2/regionalbloc/eu?fields=name;capital;population;area;flag;borders;'
+const COUNTRIES_URL = 'https://restcountries.eu/rest/v2/regionalbloc/eu?fields=name;capital;population;area;flag;borders;'
 
-const mapURL = 'https://www.google.com/maps/embed/v1/place';
+const MAP_URL = 'https://www.google.com/maps/embed/v1/place';
 const APIKEY = 'AIzaSyCnYhHe2_GKIyz_2gjHG__qxEecQ7VyLvs';
+
 const map = document.getElementById('map');
+
+const content = document.getElementById('content');
 
 function createNode(element) {
     return document.createElement(element);
 }
 
-function formatNumberEN(number) {
-    new Intl.NumberFormat('en-US').format(number)
+const formatNumberIntl = new Intl.NumberFormat('en-US');
+const formatNumberIntl3Decimals = new Intl.NumberFormat('en-US', {maximumFractionDigits: 3})
+
+function populateCountries(country) {
+    const tr = createNode('tr'),
+        tdName = createNode('td'),
+        tdCap = createNode('td'),
+        tdPop = createNode('td'),
+        tdArea = createNode('td'),
+        tdDensity = createNode('td'),
+        tdBorders = createNode('td'),
+        tdFlag = createNode('td'),
+        flagImg = createNode('img');
+
+    tdName.innerHTML = country.name;
+    tdCap.innerHTML = country.capital;
+    tdPop.innerHTML = formatNumberIntl.format(country.population);
+    tdArea.innerHTML = country.area ? formatNumberIntl.format(country.area) : 'N/A';
+    tdDensity.innerHTML = country.area ? formatNumberIntl3Decimals.format(country.population / country.area) : 'N/A';
+    tdBorders.innerHTML = country.borders.length;
+    flagImg.src = country.flag;
+
+    flagImg.addEventListener('click', e => {
+        const makeUrl = `${MAP_URL}?key=${APIKEY}&q=${country.name} ${country.capital}`;
+        map.src = makeUrl;
+    });
+
+    tdFlag.append(flagImg);
+
+    tr.append(tdName, tdCap, tdPop, tdArea, tdDensity, tdBorders, tdFlag);
+    content.append(tr);
 }
 
 function fetchCountries(apiURL) {
-    const content = document.getElementById('content');
+    
     fetch(apiURL)
         .then(response => response.json())
         .then(data => {
             data.map(country => {
-                const tr = createNode('tr'),
-                    tdName = createNode('td'),
-                    tdCap = createNode('td'),
-                    tdPop = createNode('td'),
-                    tdArea = createNode('td'),
-                    tdDensity = createNode('td'),
-                    tdBorders = createNode('td'),
-                    tdFlag = createNode('td'),
-                    flagImg = createNode('img');
-
-                tdName.innerHTML = country.name;
-                tdCap.innerHTML = country.capital;
-                tdPop.innerHTML = new Intl.NumberFormat('en-US').format(country.population);
-                tdArea.innerHTML = country.area ? new Intl.NumberFormat('en-US').format(country.area) : 'N/A';
-                tdDensity.innerHTML = country.area ? new Intl.NumberFormat('en-US', { maximumFractionDigits: 3 }).format(country.population / country.area) : 'N/A';
-                tdBorders.innerHTML = country.borders.length;
-                flagImg.src = country.flag;
-                
-                flagImg.addEventListener('click', e => {
-                    const makeUrl = `${mapURL}?key=${APIKEY}&q=${country.name} ${country.capital}`;
-                    map.src = makeUrl;
-                })
-                              
-                tdFlag.append(flagImg);
-
-                tr.append(tdName, tdCap, tdPop, tdArea, tdDensity, tdBorders, tdFlag);
-                content.append(tr);
+                populateCountries(country);
 
             })
         })
@@ -55,6 +61,8 @@ function fetchCountries(apiURL) {
 }
 
 document.addEventListener('DOMContentLoaded', e => {
-    fetchCountries(countriesURL);
+    fetchCountries(COUNTRIES_URL);
 
 })
+
+
